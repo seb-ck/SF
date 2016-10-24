44c915183dc6a969b5888a8a32697db96fcd42f5
Seb Fabre <sebastien.nj.fabre@gmail.com>
2016-10-24 21:54:48 +0200

    update gitignore

---
+++ Auth/Configuration/conf.php
@@ -0,0 +1,6 @@
+<?php
+
+$google_client_id       = '451863523152-oa6sj0b57nslthask7bdr42b665mkme2.apps.googleusercontent.com';
+$google_client_secret   = 'yfQHQM6mWqAGDP6995ESRlgH';
+$google_redirect_url    = 'http://support.crossknowledge.com/report';
+$google_developer_key   = '';
