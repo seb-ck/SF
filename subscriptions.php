f1e0eb3eab9417f642994902fe9903a4b4ae0057
Seb Fabre <sebastien.nj.fabre@gmail.com>
2016-11-30 17:29:32 +0100

    allow to delete old subscriptions to cases

--- subscriptions.php
+++ subscriptions.php
@@ -15,23 +15,23 @@ require_once (SOAP_CLIENT_BASEDIR.'/SforceHeaderOptions.php');
 
 /*
 	Case fields:
-	ID	ISDELETED	CASENUMBER	CONTACTID	ACCOUNTID	COMMUNITYID	PARENTID	SUPPLIEDNAME	SUPPLIEDEMAIL	SUPPLIEDPHONE	SUPPLIEDCOMPANY	TYPE	RECORDTYPEID	STATUS	REASON	ORIGIN	SUBJECT	PRIORITY	DESCRIPTION	ISCLOSED	CLOSEDDATE	ISESCALATED	OWNERID	CREATEDDATE	CREATEDBYID	LASTMODIFIEDDATE	LASTMODIFIEDBYID	SYSTEMMODSTAMP	LASTVIEWEDDATE	LASTREFERENCEDDATE	CREATORFULLPHOTOURL	CREATORSMALLPHOTOURL	CREATORNAME	
+	ID	ISDELETED	CASENUMBER	CONTACTID	ACCOUNTID	COMMUNITYID	PARENTID	SUPPLIEDNAME	SUPPLIEDEMAIL	SUPPLIEDPHONE	SUPPLIEDCOMPANY	TYPE	RECORDTYPEID	STATUS	REASON	ORIGIN	SUBJECT	PRIORITY	DESCRIPTION	ISCLOSED	CLOSEDDATE	ISESCALATED	OWNERID	CREATEDDATE	CREATEDBYID	LASTMODIFIEDDATE	LASTMODIFIEDBYID	SYSTEMMODSTAMP	LASTVIEWEDDATE	LASTREFERENCEDDATE	CREATORFULLPHOTOURL	CREATORSMALLPHOTOURL	CREATORNAME
 	THEME__C	PRODUCT_VERSION__C	ACCOUNT_NAME_TEMP__C	CASE_IMPORT_ID__C	SPIRA__C	MANTIS__C	CONTACT_EMAIL_IMPORT__C	GEOGRAPHICAL_ZONE__C	NO_TYPE_REFRESH__C	ACTIVITY__C	BACK_IN_QUEUE__C	TIMESPENT_MN__C	SURVEY_SENT__C	MOST_RECENT_REPLY_SENT__C	MOST_RECENT_INCOMING_EMAIL__C	NEW_EMAIL__C	REQUEST_TYPE__C	URL__C	LOGIN__C	PASSWORD__C	BROWSER__C	REPRODUCTION_STEP__C	ASSOCIATED_DEADLINE__C	RELATED_TICKET__C	CC__C	CATEGORIES__C	BILLABLE__C	LANGUAGE__C	KAYAKO_ID__C	COMMENTAIRE_SURVEY__C	IDSURVEY__C	TIME_SPENT_BILLABLE__C	SURVEY_SENT_DATE__C	CONTACT_EMAIL_FOR_INTERNAL_USE__C	OPENED_ON_BEHALF_CUSTOMER__C	BU__C
 
 	EmailMessage fields:
-	ID	PARENTID	ACTIVITYID	CREATEDBYID	CREATEDDATE	LASTMODIFIEDDATE	LASTMODIFIEDBYID	SYSTEMMODSTAMP	TEXTBODY	HTMLBODY	HEADERS	SUBJECT	FROMNAME	FROMADDRESS	TOADDRESS	CCADDRESS	BCCADDRESS	INCOMING	HASATTACHMENT	STATUS	MESSAGEDATE	ISDELETED	REPLYTOEMAILMESSAGEID	ISEXTERNALLYVISIBLE																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
+	ID	PARENTID	ACTIVITYID	CREATEDBYID	CREATEDDATE	LASTMODIFIEDDATE	LASTMODIFIEDBYID	SYSTEMMODSTAMP	TEXTBODY	HTMLBODY	HEADERS	SUBJECT	FROMNAME	FROMADDRESS	TOADDRESS	CCADDRESS	BCCADDRESS	INCOMING	HASATTACHMENT	STATUS	MESSAGEDATE	ISDELETED	REPLYTOEMAILMESSAGEID	ISEXTERNALLYVISIBLE
 
 
 */
 
-try 
+try
 {
   $mySforceConnection = new SforcePartnerClient();
   $mySoapClient = $mySforceConnection->createConnection(SOAP_CLIENT_BASEDIR.'/partner.wsdl.xml');
   $mylogin = $mySforceConnection->login($USERNAME, $PASSWORD);
 
-	$query = "SELECT ParentId, SubscriberId FROM EntitySubscription";
-	//$query = "SELECT ParentId, SubscriberId FROM EntitySubscription WHERE SubscriberId='00524000000oP8K'"; // ID = SEB
+	$query = "SELECT Id, ParentId, SubscriberId FROM EntitySubscription WHERE Parent.type='Case'";
+	//$query = "SELECT Id, ParentId, SubscriberId FROM EntitySubscription WHERE Parent.type='Case' AND SubscriberId='00524000000oP8K'"; // ID = SEB
 	$response = $mySforceConnection->query($query);
 	$parentIds = '';
 	$casesIds = array();
@@ -42,32 +42,28 @@ try
 	$accounts = array();
 	$accountIds = array();
 	$now = new DateTime();
-	
-	foreach ($response as $record) 
-	{
-		$casesIds []= $record->ParentId;
-	}
-	
+  $oldCases = array();
+
 	foreach ($response as $result)
 	{
-		$cases[$result->Id] = $result;
-		
+		$casesIds []= $result->ParentId;
+
 		if ($result->fields->SubscriberId)
 		{
 			$ownerIds[$result->fields->SubscriberId] = false;
-			$casesPerOwner[$result->fields->SubscriberId] []= $result->ParentId;
+			$casesPerOwner[$result->fields->SubscriberId][$result->Id] = $result->ParentId;
 		}
 	}
-	
+
 	$owners = $mySforceConnection->retrieve('Alias', 'User', array_keys($ownerIds));
 	foreach ($owners as $o)
 	{
 		if (!empty($o->fields->Alias))
 			$ownerIds[$o->Id] = $o->fields->Alias;
 	}
-	
+
 	asort($ownerIds);
-	
+
 	foreach ($ownerIds as $ownerId => $owner)
 	{
 		echo '<table id="results">';
@@ -84,63 +80,81 @@ try
 		echo '	<th>Last modification date</th>';
 		echo '</tr>';
 		echo '</thead>';
-		
+
 		echo '<tbody>';
-		
-		$fetchCases = $mySforceConnection->retrieve('Id, Subject, CaseNumber, Status, OwnerId, LastModifiedDate, CreatedDate', 'Case', $casesPerOwner[$ownerId]);
-		
+
+		$fetchCases = $mySforceConnection->retrieve('Id, Subject, CaseNumber, Status, OwnerId, LastModifiedDate, CreatedDate', 'Case', array_values($casesPerOwner[$ownerId]));
+
 		$cases = array();
 		foreach ($fetchCases as $c)
 		{
 			if (empty($c->type))
 				continue;
-			
+
 			$cases[$c->Id] = $c;
-			
+
 			if (empty($ownerIds[$c->fields->OwnerId]))
 			{
 				$owners = $mySforceConnection->retrieve('Alias', 'User', array($c->fields->OwnerId));
-				$ownerIds[$c->fields->OwnerId] = reset($owners)->fields->Alias;
+        if (!empty($owners) && reset($owners)->fields)
+				    $ownerIds[$c->fields->OwnerId] = reset($owners)->fields->Alias;
 			}
 		}
-		
+
 		$cpt = 0;
-		foreach ($casesPerOwner[$ownerId] as $id)
+		foreach ($casesPerOwner[$ownerId] as $subs => $id)
 		{
 			if (empty($cases[$id]))
 				continue;
-		
+
 			$c = $cases[$id];
-			
+
 			$tr = '';
-			
+
 			$tr .= '<td><a href="https://eu5.salesforce.com/' . $c->Id . '">' . $c->fields->CaseNumber . '</a></td>';
 			$tr .= '<td>' . $c->fields->Subject . '</td>';
 			$tr .= '<td>' . $c->fields->Status . '</td>';
-			
-			$tr .= '<td>' . $ownerIds[$c->fields->OwnerId] . '</td>';
-			
+
+      if (!empty($ownerIds[$c->fields->OwnerId]))
+			   $tr .= '<td>' . $ownerIds[$c->fields->OwnerId] . '</td>';
+      else
+        $tr .= '<td title="Owner not from the support team">?</td>';
+
 			$tr .= '<td>' . str_replace(array('T', '.000Z'), array(' ', ''), $c->fields->CreatedDate) . '</td>';
 			$tr .= '<td>' . str_replace(array('T', '.000Z'), array(' ', ''), $c->fields->LastModifiedDate) . '</td>';
-			
+
 			$tr .= '</tr>';
-			
+
 			if ($cpt++%2)
 				$tr = '<tr class="odd">' . $tr;
 			else
 				$tr = '<tr>' . $tr;
-			
-			echo $tr;
+
+      $lastModifiedDate = DateTime::createFromFormat('Y-m-d\TH:i:s.000\Z', $c->LastModifiedDate);
+      $diff = $lastModifiedDate->diff($now)->days;
+
+      if ($diff > 30)
+        $oldCases []= $subs;
+      else
+			   echo $tr;
 		}
-		
+
 		echo '</tbody>';
 		echo '</table>';
 		echo '<br/>';
 	}
-	
+
+  //print_r($oldCases);die;
+
+  if (!empty($_GET['purge']))
+  {
+    if (!empty($oldCases))
+      $mySforceConnection->delete($oldCases);
+  }
+
 	echo '<br/><br/><br/>';
-} 
-catch (Exception $e) 
+}
+catch (Exception $e)
 {
   var_dump($e);
 }
