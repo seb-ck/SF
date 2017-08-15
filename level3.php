1a52ebaab1938b7ddbe2d323db2a9f8be9f730a9
Seb Fabre <sebastien.nj.fabre@gmail.com>
2017-08-15 21:02:57 +0200

    add level 3 backlog dashboard

---
+++ level3.php
@@ -0,0 +1,97 @@
+<?php
+require_once('bootstrap.php');
+require_once('head.php');
+
+?>
+
+  <h1>Level 3 backlog</h1>
+
+<?php
+
+try
+{
+	$query = "SELECT Status__c, CreatedBy.Alias, Minutes__c, Owner__r.Alias,
+                Case__r.CaseNumber, Case__r.Subject, Case__r.Owner.Alias, Case__r.Status, Case__r.IsEscalated,
+                Case__r.Account.Name, Case__r.CreatedDate
+            FROM Timesheet__c
+            WHERE ((Status__c != '' AND Status__c != '-') OR Owner__r.Alias = 'SF')
+              AND Status__c != 'Completed' AND Status__c != 'Cancelled'
+            ORDER BY Owner__r.Alias";
+
+	$response = $mySforceConnection->query($query);
+	$timesheets = array();
+	$owners = array();
+	$now = new DateTime();
+
+  foreach ($response as $record)
+  {
+    $owner = !empty($record->fields->Owner__r->fields->Alias) ? $record->fields->Owner__r->fields->Alias : $record->fields->CreatedBy->fields->Alias;
+    $timesheets[$owner] []= $record;
+  }
+
+	foreach (array_keys($timesheets) as $owner)
+	{
+		$assigned = count($timesheets[$owner]);
+
+		echo '<table id="results">';
+		echo '<thead>';
+		echo '<tr>';
+		echo '	<th colspan=11 align=left><span style="font-size: 1.5em">' . $owner . '</span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;(' . $assigned . ' pending tasks)</th>';
+		echo '</tr>';
+		echo '<tr>';
+		echo '	<th>Case Number</th>';
+		echo '	<th>Subject</th>';
+		echo '	<th>Case Status</th>';
+		echo '	<th>Account</th>';
+		echo '	<th>Creation date</th>';
+		echo '	<th>Task status</th>';
+		echo '	<th>Time spent</th>';
+		echo '</tr>';
+		echo '</thead>';
+
+		echo '<tbody>';
+
+		$cpt = 0;
+		foreach ($timesheets[$owner] as $ts)
+		{
+      $c = $ts->fields->Case__r;
+			$isEscalated = ($c->fields->IsEscalated == 'true');
+
+			$tr = '';
+
+			$escalated = '';
+			if ($isEscalated)
+				$escalated = '&nbsp;<img src="https://eu5.salesforce.com/img/func_icons/util/escalation12.gif" />';
+
+			$tr .= '<td><a href="https://eu5.salesforce.com/' . $c->Id . '">' . $c->fields->CaseNumber . '</a>' . $escalated . '</td>';
+			$tr .= '<td>' . $c->fields->Subject . '</td>';
+			$tr .= '<td>' . $c->fields->Status . '</td>';
+
+			if (!empty($c->fields->Account->fields->Name))
+				$tr .= '<td>' . $c->fields->Account->fields->Name . '</td>';
+			else
+				$tr .= '<td></td>';
+
+			$tr .= '<td>' . str_replace(array('T', '.000Z'), array(' ', ''), $c->fields->CreatedDate) . '</td>';
+      $tr .= '<td>' . $ts->fields->Status__c . '</td>';
+			$tr .= '<td>' . $ts->fields->Minutes__c . '</td>';
+
+			$tr .= '</tr>';
+
+			if ($cpt++%2)
+				$tr = '<tr class="odd">' . $tr;
+			else
+				$tr = '<tr>' . $tr;
+
+			echo $tr;
+		}
+
+		echo '</tbody>';
+		echo '</table>';
+		echo '<br/>';
+	}
+}
+catch (Exception $e)
+{
+  var_dump($e);
+}
